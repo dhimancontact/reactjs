011216b76e05fe4fa590f491c7e32283d05e6ff7
anilkumar <anilkumar.dhiman1@gmail.com>
2016-10-31 21:16:10 +0530

    react practice

---
+++ layout_foot.php
@@ -0,0 +1,20 @@
+</div>
+<!-- /container -->
+
+<!-- react js -->
+<script src="libs/js/react.js"></script>
+<script src="libs/js/react-dom.js"></script>
+<script src="libs/js/browser.min.js"></script>
+
+<!-- main react components -->
+<script type="text/babel" src="libs/js/react/main.js"></script>
+
+<!-- jQuery library -->
+<script src="libs/js/jquery.js"></script>
+
+<!-- bootstrap JavaScript -->
+<script src="libs/css/bootstrap/js/bootstrap.min.js"></script>
+<!--<script src="libs/css/bootstrap/docs-assets/js/holder.js"></script>-->
+
+</body>
+</html>
